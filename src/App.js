2a1adf1816f0cede7235080644c4dbb1b5289691
mehdi <mehdi.khorrami78@gmail.com>
2022-10-15 11:52:16 +0330

    creat Component and fix routing app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { BrowserRouter as Router ,  Routes , Route } from "react-router-dom";
+import Home from "./component/Home";
+import Details from "./component/Details";
+import Login from "./component/Login";
 
 function App() {
   return (
-    <div >
-        
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="Details" element={<Details />}/>
+            <Route path="Login" element={<Login />}/>
+            <Route path="*" element={<Home />}/>
+        </Routes>
+    </Router>
   );
 }
 
