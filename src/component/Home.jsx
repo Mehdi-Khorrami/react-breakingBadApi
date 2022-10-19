07041a91833f8a08eb338b6fa67e5c631e7598f3
mehdi <mehdi.khorrami78@gmail.com>
2022-10-19 09:16:31 +0330

    create List Data by Axios

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,35 +1,69 @@
-import  Box  from '@mui/system/Box'
-import  AppBar  from '@mui/material/AppBar'
+import '../App.css'
+import Box from '@mui/system/Box'
+import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router';
+import axios from "axios"
+import {  Grid, Paper } from '@mui/material';
 
 function Home() {
   return (
     <Box>
-        <Header />
+      <Header />
+      <Box className='card'>
+        <Body />
+      </Box>
     </Box>
   )
 }
 
 
 const Header = () => {
-    const navigat = useNavigate();
+  const navigat = useNavigate();
 
-    return (
-        <Box sx={{ flexGrow: 1  }} >
-        <AppBar position="static" sx={{ background : "#3B3B3B" }}>
-          <Toolbar>
-            <Typography variant="h5" component="h1" sx={{ flexGrow: 1 }}>
-               The Breaking Bad API
-            </Typography>
-            <Button color="inherit" onClick={() => navigat('/Login')} >Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    )
+  return (
+    <Box sx={{ flexGrow: 1 }} >
+      <AppBar position="static" sx={{ background: "#3B3B3B" }}>
+        <Toolbar>
+          <Typography variant="h5" component="h1" sx={{ flexGrow: 1 }}>
+            The Breaking Bad API
+          </Typography>
+          <Button color="inherit" onClick={() => navigat('/Login')} >Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+
+const Body = () => {
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://www.breakingbadapi.com/api/characters?limit=12&offset=1')
+      .then((res) => {
+        setList(res.data)
+      })
+      .catch(error => console.log('Error : ' + { error }))
+  }, [])
+
+
+  return (
+    list.map((info, index) => {
+      return (
+          <Grid key={index}>
+            <Paper>
+              <img src={info.img} alt={info.category} />
+              <h4>{info.name}</h4>
+            </Paper>
+          </Grid>
+      )
+    })
+  )
 }
 
 export default Home
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,9 +1,33 @@
+import { Box, Container, Grid, Paper } from '@mui/material'
 import React from 'react'
 
 function Login() {
+
+  const theme1 = {
+    border:"1px solid red",
+    height :"100vh",
+    display :'flex',
+    alignItems:'center' ,
+  }
+  const theme2 = {
+    border:"1px solid blue",
+    width: '30% ' ,
+    height: '500px',
+    margin:'100px auto'
+  }
+
+
   return (
-    <div>Login</div>
+  
+      <Grid  container spacing={2}>
+        <Grid md={8} xs={12}>
+            <Paper  sx={theme2}  elevation={8} >
+
+            </Paper> 
+        </Grid>
+      </Grid>
+ 
   )
 }
 
-export default Login
+export default Login 
