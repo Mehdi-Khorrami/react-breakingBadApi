6e2c82985ee0beaf2e71f0eb0eb690c9133dbba5
mehdi <mehdi.khorrami78@gmail.com>
2022-10-26 08:47:14 +0330

    refactor : refactor details commponent ( set data when refresh)

--- src/page/Details.jsx
+++ src/page/Details.jsx
@@ -1,4 +1,5 @@
 
+import { CircularProgress,Box} from '@mui/material'
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
@@ -9,37 +10,39 @@ function Details() {
   const item = useContext(selected)
   const [lists, setLists] = useState([]);
   const url = useParams()
+  const finalItem = item.details ? item.details : lists[0];
 
   useEffect(() => {
-    axios.get('https://www.breakingbadapi.com/api/characters')
+    axios.get('https://www.breakingbadapi.com/api/characters/' + url.id)
       .then((res) => {
         setLists(res.data)
       })
       .catch(error => console.log('Error : ' + { error }))
   }, [])
 
-
-
+  if (!finalItem) {
+    return <Box display='flex' alignItems='center' justifyContent='center' height='100vh' width='100%'>
+      <CircularProgress />
+    </Box>;
+  }
   return (
     <section className='details'>
       <div>
-        <img src={lists[url.id].img} alt="" />
+        <img src={finalItem.img} alt="" />
         <div>
           <h1>Details</h1>
           <h4>name :</h4>
-          <p>{ lists[url.id].name} </p>
+          <p>{finalItem.name} </p>
           <h4>birthday :</h4>
-          <p>{lists[url.id].birthday}</p>
+          <p>{finalItem.birthday}</p>
           <h4>occupation :</h4>
-          <p>{ lists[url.id].occupation[0]}</p>
+          <p>{finalItem.occupation[0]}</p>
           <h4>nickname : </h4>
-          <p>{ lists[url.id].nickname} </p>
+          <p>{finalItem.nickname} </p>
         </div>
       </div>
     </section>
   )
 }
-
-
 
 export default Details
